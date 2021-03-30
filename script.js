eaa1a04658acb1a87623455550fb6711dc42c5ba
KNJour <KNJournell@gmail.com>
2021-03-30 16:31:50 -0400

    weather project

---
+++ script.js
@@ -0,0 +1,12 @@
+
+// var pTag = document.querySelectorAll("p");
+// function getColor(element) {
+//     console.log(element.value);
+//     for (var i = 0; i < pTag.length; i++) {
+//         pTag[i].style.color = element.value;
+//     }
+// }
+
+function removeIt() {
+    document.getElementById("cookies").style.display = "none";
+}
